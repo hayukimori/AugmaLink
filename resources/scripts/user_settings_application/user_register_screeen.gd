bfdc32056a53147c4ce1e595e86129014835e36e
hayukimori <hayukimori@gmail.com>
2025-03-15 19:22:51 -0300

    updated: user register/settings screen

--- resources/scripts/user_settings_application/user_register_screeen.gd
+++ resources/scripts/user_settings_application/user_register_screeen.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var accent_color_picker: ColorPickerButton = $AccentColorPicker
+@onready var done_btn: Button = $DoneBtn
 
 
 func _ready() -> void:
@@ -13,6 +14,12 @@ func _on_accent_color_picker_color_changed(color: Color) -> void:
 	var focus_stylebox: StyleBoxFlat = accent_color_picker.get_theme_stylebox("focus")
 	var pressed_stylebox: StyleBoxFlat = accent_color_picker.get_theme_stylebox("pressed")
 	
+	var done_btn_normal_stylebox: StyleBoxFlat = done_btn.get_theme_stylebox("normal")
+	var done_btn_hover_stylebox: StyleBoxFlat = done_btn.get_theme_stylebox("hover")
+	var done_btn_focus_stylebox: StyleBoxFlat = done_btn.get_theme_stylebox("focus")
+	var done_btn_pressed_stylebox: StyleBoxFlat = done_btn.get_theme_stylebox("pressed")
+	
+	# Accent color BTN
 	normal_stylebox.bg_color = color
 	normal_stylebox.border_color = color
 
@@ -24,3 +31,21 @@ func _on_accent_color_picker_color_changed(color: Color) -> void:
 
 	pressed_stylebox.bg_color = color
 	pressed_stylebox.border_color = color
+	
+
+	 # Done BTN
+	done_btn_normal_stylebox.bg_color = color
+	done_btn_normal_stylebox.border_color = color
+
+	done_btn_hover_stylebox.bg_color = color
+	done_btn_hover_stylebox.border_color = color
+
+	done_btn_focus_stylebox.bg_color = color
+	done_btn_focus_stylebox.border_color = color
+
+	done_btn_pressed_stylebox.bg_color = color
+	done_btn_pressed_stylebox.border_color = color
+
+
+func _on_done_btn_pressed() -> void:
+	pass
